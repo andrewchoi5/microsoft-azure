3ea44e30f633a40844dec3a18bb268a86276bdcd
Andrew Choi <andrewchoi5@hotmail.com>
2019-09-17 18:01:55 -0700

    Update customScriptv4.sh

--- customScriptv4.sh
+++ customScriptv4.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 apt-get install -y nginx
-echo "Kafka Running....! " $HOSTNAME "!" 
-echo "!!!!!!!!!An Azure Custom Extension Script is running now....! " $HOSTNAME "!" | sudo tee /var/www/html/index.html
+echo "Kafka Running....! " $HOSTNAME "!"  | sudo tee /var/www/html/index.html
 
 
 # # Install Oracle Java
