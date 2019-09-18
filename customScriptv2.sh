a36711ed9fa7bf16655b5a0125ecc00ab950ddd8
Andrew Choi <andrewchoi5@hotmail.com>
2019-09-17 17:52:57 -0700

    Update customScriptv2.sh

--- customScriptv2.sh
+++ customScriptv2.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 apt-get install -y nginx
-echo "Installing Kafka Server now....! " $HOSTNAME "!"
+echo "Installing Kafka Server now....! " $HOSTNAME "!" | sudo tee /var/www/html/index.html
 echo "An Azure Custom Extension Script is running now....! " $HOSTNAME "!" | sudo tee /var/www/html/index.html
 
 
-# Install Oracle Java
-echo "Installing Java"
+# # Install Oracle Java
+# echo "Installing Java"
 
-# redhat java install
-# cd /tmp
-# wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm"
-# wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm"
+# # redhat java install
+# # cd /tmp
+# # wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm"
+# # wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm"
 
-#Install packages
-# rpm -Uvh jdk-8u102-linux-x64.rpm
-# rpm -Uvh jre-8u102-linux-x64.rpm
+# #Install packages
+# # rpm -Uvh jdk-8u102-linux-x64.rpm
+# # rpm -Uvh jre-8u102-linux-x64.rpm
 
-# Install kafka
-echo "Installing Kafka"
-name=kafka
-kafkaversion=2.10
-description="Apache Kafka is a distributed publish-subscribe messaging system."
-url="https://kafka.apache.org/"
-arch="all"
-section="misc"
-license="Apache Software License 2.0"
-package_version="-1"
-download_url=http://mirrors.ukfast.co.uk/sites/ftp.apache.org/kafka/2.3.0/kafka_2.11-2.3.0.tgz
+# # Install kafka
+# echo "Installing Kafka"
+# name=kafka
+# kafkaversion=2.10
+# description="Apache Kafka is a distributed publish-subscribe messaging system."
+# url="https://kafka.apache.org/"
+# arch="all"
+# section="misc"
+# license="Apache Software License 2.0"
+# package_version="-1"
+# download_url=http://mirrors.ukfast.co.uk/sites/ftp.apache.org/kafka/2.3.0/kafka_2.11-2.3.0.tgz
 
 
 
