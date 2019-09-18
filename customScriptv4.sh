379129364cac8635d7762838e660419059a9ea22
Andrew Choi <andrewchoi5@hotmail.com>
2019-09-17 17:58:51 -0700

    Update and rename customScriptv3.sh to customScriptv4.sh

--- customScriptv4.sh
+++ customScriptv4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt-get install -y nginx
+echo "Kafka Running....! " $HOSTNAME "!" 
 echo "!!!!!!!!!An Azure Custom Extension Script is running now....! " $HOSTNAME "!" | sudo tee /var/www/html/index.html
 
 
